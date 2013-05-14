a248523f79ea98f2d7e129b6f55d069c99650440
Michael L Perry <michael@qedcode.com>
2013-05-14 12:02:27 -0500

    Created an http handler for the distributor.

---
+++ Correspondence.Distributor.Web/App_Code/BinaryHttpHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Correspondence.Distributor.Web
+{
+    public class BinaryHttpHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("I'm here!");
+        }
+    }
+}
