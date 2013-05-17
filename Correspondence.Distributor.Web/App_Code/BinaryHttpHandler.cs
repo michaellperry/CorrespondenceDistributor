6dce74b59efb3764b2df010cb32b2601f07484ad
Michael L Perry <michael@qedcode.com>
2013-05-16 23:51:00 -0500

    Call distributor service from binary HTTP handler.

--- Correspondence.Distributor.Web/App_Code/BinaryHttpHandler.cs
+++ Correspondence.Distributor.Web/App_Code/BinaryHttpHandler.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using Correspondence.Distributor.BinaryHttp;
+using UpdateControls.Correspondence;
+using UpdateControls.Correspondence.Mementos;
 
 namespace Correspondence.Distributor.Web
 {
     public class BinaryHttpHandler : IHttpHandler
     {
+        private DistributorService _service;
+
+        public BinaryHttpHandler()
+        {
+            _service = new DistributorService();
+        }
+
         public bool IsReusable
         {
             get { return true; }
@@ -14,7 +24,105 @@ namespace Correspondence.Distributor.Web
 
         public void ProcessRequest(HttpContext context)
         {
-            context.Response.Write("I'm here!");
+            var reader = new BinaryReader(context.Request.InputStream);
+            BinaryRequest request = BinaryRequest.Read(reader);
+
+            BinaryResponse response =
+                TryHandle<GetManyRequest>           (request, GetMany) ??
+                TryHandle<PostRequest>              (request, Post) ??
+                TryHandle<InterruptRequest>         (request, Interrupt) ??
+                TryHandle<NotifyRequest>            (request, Notify) ??
+                TryHandle<WindowsSubscribeRequest>  (request, WindowsSubscribe) ??
+                TryHandle<WindowsUnsubscribeRequest>(request, WindowsUnsubscribe);
+            if (response == null)
+                throw new CorrespondenceException(String.Format("Unknown request type {0}.", request));
+            var writer = new BinaryWriter(context.Response.OutputStream);
+            response.Write(writer);
+            writer.Flush();
+        }
+
+        private BinaryResponse TryHandle<TRequest>(BinaryRequest request, Func<TRequest, BinaryResponse> method)
+            where TRequest : BinaryRequest
+        {
+            TRequest specificRequest = request as TRequest;
+            if (specificRequest != null)
+                return method(specificRequest);
+            else
+                return null;
+        }
+
+        private GetManyResponse GetMany(GetManyRequest request)
+        {
+            var pivotIds = request.PivotIds
+                .ToDictionary(p => p.FactId, p => p.TimestampId);
+            FactTreeMemento factTree = _service.GetMany(
+                request.ClientGuid, 
+                request.Domain, 
+                request.PivotTree, 
+                pivotIds, 
+                request.TimeoutSeconds);
+            return new GetManyResponse
+            {
+                FactTree = factTree,
+                PivotIds = pivotIds
+                    .Select(pair => new FactTimestamp
+                    {
+                        FactId = pair.Key,
+                        TimestampId = pair.Value
+                    })
+                    .ToList()
+            };
+        }
+
+        private PostResponse Post(PostRequest request)
+        {
+            _service.Post(
+                request.ClientGuid,
+                request.Domain,
+                request.MessageBody,
+                request.UnpublishedMessages);
+            return new PostResponse();
+        }
+
+        private InterruptResponse Interrupt(InterruptRequest request)
+        {
+            _service.Interrupt(
+                request.ClientGuid,
+                request.Domain);
+            return new InterruptResponse();
+        }
+
+        private NotifyResponse Notify(NotifyRequest request)
+        {
+            _service.Notify(
+                request.ClientGuid,
+                request.Domain,
+                request.PivotTree,
+                request.PivotId,
+                request.Text1,
+                request.Text2);
+            return new NotifyResponse();
+        }
+
+        private WindowsSubscribeResponse WindowsSubscribe(WindowsSubscribeRequest request)
+        {
+            _service.WindowsSubscribe(
+                request.ClientGuid,
+                request.Domain,
+                request.PivotTree,
+                request.PivotId,
+                request.DeviceUri);
+            return new WindowsSubscribeResponse();
+        }
+
+        private WindowsUnsubscribeResponse WindowsUnsubscribe(WindowsUnsubscribeRequest request)
+        {
+            _service.WindowsUnsubscribe(
+                request.Domain,
+                request.PivotTree,
+                request.PivotId,
+                request.DeviceUri);
+            return new WindowsUnsubscribeResponse();
         }
     }
 }
---
+++ Correspondence.Distributor/DistributorService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpdateControls.Correspondence.Mementos;
+
+namespace Correspondence.Distributor
+{
+    public class DistributorService
+    {
+        public FactTreeMemento GetMany(
+            string clientGuid, 
+            string domain, 
+            FactTreeMemento pivotTree, 
+            Dictionary<long, long> pivotIds, 
+            int timeoutSeconds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Post(
+            string clientGuid, 
+            string domain, 
+            FactTreeMemento factTree, 
+            List<UnpublishMemento> unpublishMessages)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Interrupt(
+            string clientGuid,
+            string domain)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Notify(
+            string clientGuid,
+            string domain, 
+            FactTreeMemento pivotTree, 
+            long pivotId, 
+            string text1, 
+            string text2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WindowsSubscribe(
+            string clientGuid,
+            string domain,
+            FactTreeMemento pivotTree,
+            long pivotId,
+            string deviceUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WindowsUnsubscribe(
+            string domain,
+            FactTreeMemento pivotTree,
+            long pivotId,
+            string deviceUri)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
