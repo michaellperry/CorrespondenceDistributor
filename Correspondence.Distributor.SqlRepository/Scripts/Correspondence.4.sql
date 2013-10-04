7c17dd014adea6f926ee5b30b7b8f25bae00daab
Michael L Perry <michael@qedcode.com>
2013-10-04 16:19:04 -0500

    Add ancestor to the primary key of the message.

---
+++ Correspondence.Distributor.SqlRepository/Scripts/Correspondence.4.sql
@@ -0,0 +1,9 @@
+﻿ALTER TABLE [Message]
+	DROP CONSTRAINT PK__Message
+
+ALTER TABLE [Message]
+    ADD CONSTRAINT PK__Message PRIMARY KEY CLUSTERED
+	    (PivotId, FactId, AncestorFactId, AncestorRoleId)
+
+CREATE NONCLUSTERED INDEX IX_Message_Fact ON [Message]
+    (FactId ASC)
